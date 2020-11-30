f15f9f1f7d7a15b69341fbec3ba66e93b69d2fc6
Rashmi876 <43865823+Rashmi876@users.noreply.github.com>
2020-11-29 19:29:50 -0500

    My javascript attempt for Round Robin
    The script is commented out because it does not work when clicked. Not sure how to get it to work to start the animation

---
+++ scripts/rr.js
@@ -0,0 +1,20 @@
+//First attempt for button
+// $(document).ready(function () {
+
+//   $(".start").click(function() {
+//     var nodePosition = $(this).position();
+//     $(".process1").animate( {
+//         top: nodePosition.top + 30
+//       }, 1000);
+//   });
+
+// });
+
+
+// Second attept for button
+// document.getElementById('start').addEventListener('click',function() {
+// var c = document.getElementsByClassName('process_column');
+// for (var i = 0; i < c.length; i++) {
+// c[i].classList.add('animate');
+// }
+// })
