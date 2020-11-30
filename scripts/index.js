092182f8557d95c80d8c06c0ae73c91288760779
Paul Smith <paulrsmithjnr@gmail.com>
2020-11-30 05:12:17 -0500

    Completed FCFS

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,19 @@
 $(document).ready(function() {
     $("main").load("../templates/home.html");
     
+    $("#navbarLogo").click(function() {
+        event.preventDefault();
+        $("main").load("../templates/home.html");
+    });
+
     $("#sjf").click(function() {
         event.preventDefault();
         $("main").load("../templates/sjf.html");        
+    });
+
+    $("#fcfs").click(function() {
+        event.preventDefault();
+        $("main").load("../templates/fcfs.html");        
     });
 
     $("#srt").click(function() {
