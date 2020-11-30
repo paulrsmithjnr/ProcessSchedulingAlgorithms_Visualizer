0a1729029dc77edd4bd732ffe0cf129226a39e81
unknown <dawkins.dania439@gmail.com>
2020-11-30 04:22:17 -0500

    Minor changes to FCFS, JS not working

---
+++ scripts/fcfs.js
@@ -0,0 +1,14 @@
+function move() {
+  var elem = document.getElementByClassName("fprocess1");   
+  var pos = 0;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++; 
+      elem.style.top = pos + 'px'; 
+      elem.style.left = pos + 'px'; 
+    }
+  }
+}
